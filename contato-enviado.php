a7c53fc3faf0ff372e4ab8d7be73987eab9d3397
Gustavo Salibi <gustavohstorti@gmail.com>
2018-02-03 23:31:25 -0200

    Fontes definidas

--- contato-enviado.php
+++ contato-enviado.php
@@ -1,25 +1,24 @@
 <link rel="stylesheet" type="text/css" href="styles/fontes.css"/>
 <style type="text/css">
-.form-message {
+
+p,a {
 	font-family: 'Nunito', sans-serif;
+	color: #fff;
+	text-align: center;
+	font-weight: 700;
+}
+
+.form-message {
 	max-width: 350px;
 	margin: 120px auto 15px auto;
 	padding: 10px 20px;
 	border-radius: 8px;
-	text-align: center;
 	font-size: 20px;
-	color: #fff;
-	font-weight: 700;
 	background: rgba(0,0,0,0.3);
 }
 
 .form-new-message {
-	display: block;
-	font-family: 'Nunito', sans-serif;
-	text-align: center;
 	font-size: 16px;
-	color: #fff;
-	font-weight: 700;
 }
 </style>
 
